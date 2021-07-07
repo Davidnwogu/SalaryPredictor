94317bf621544bf60d60bf91f57038abf3b8ba76
David Nwogu <ajuri.nwogu@gmail.com>
2021-07-07 11:48:21 -0500

    uploaded scraper

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  5 19:44:37 2021
+
+@author: dav3
+"""
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "/Users/dav3/Dev/SalaryPredictor/chromedriver"
+
+df = gs.get_jobs('data scientist', 15, False, path, 15)
